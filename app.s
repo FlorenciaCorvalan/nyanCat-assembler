524329d22edae3872872215d7e3cd0c9c3ce5a8d
Ramiro <rampare99@gmail.com>
2021-06-09 18:50:45 -0300

    Agregada tarta

--- app.s
+++ app.s
@@ -7,235 +7,95 @@
 
 main:
 	// X0 contiene la direccion base del framebuffer
- 	mov x23, x0	// Save framebuffer base address to x20
+	mov x23, x0	// Save framebuffer base address to x23
+        mov x19, #0
 	
 	//COLORES
-	movz x10, 0x01, lsl 16
-	movk x10, 0x3368, lsl 00  // Azul
-	
-	movz x11, 0x99, lsl 16
-	movk x11, 0x989A, lsl 00  // Gris
-
-	movz x12, 0xFF, lsl 16
-	movk x12, 0xCB9B, lsl 00  // Tarta
-	
-	movz x13, 0xFF, lsl 16
-	movk x13, 0x98FF, lsl 00  // Rosa
-
-	movz x14, 0xFE, lsl 16
-	movk x14, 0x329E, lsl 00  // Cereza
-	
-	movz x15, 0xFF, lsl 16
-	movk x15, 0x0000, lsl 00  // Rojo
-
-	movz x16, 0xFF, lsl 16
-	movk x16, 0x9101, lsl 00  // Naranja
-	
-	movz x17, 0xFF, lsl 16
-	movk x17, 0xFF00, lsl 00  // Amarillo
-	
-	movz x18, 0x34, lsl 16
-	movk x18, 0xFF00, lsl 00  // Verde
-	
-	movz x19, 0x01, lsl 16
-	movk x19, 0x98FF, lsl 00  // Celeste
-	
-	movz x20, 0x68, lsl 16
-	movk x20, 0x32FF, lsl 00  // Violeta
-	
-	movz x21, 0x00, lsl 16
-	movk x21, 0x0000, lsl 00  // Negro
-	
-	movz x22, 0xFF, lsl 16
-	movk x22, 0xFFFF, lsl 00  // Blanco
+//	movz x10, 0x01, lsl 16
+//	movk x10, 0x3368, lsl 00  // Azul
+//	
+//	movz x11, 0x99, lsl 16
+//	movk x11, 0x989A, lsl 00  // Gris
+//
+//	movz x12, 0xFF, lsl 16
+//	movk x12, 0xCB9B, lsl 00  // Tarta
+//	
+//	movz x13, 0xFF, lsl 16
+//	movk x13, 0x98FF, lsl 00  // Rosa
+//
+//	movz x14, 0xFE, lsl 16
+//	movk x14, 0x329E, lsl 00  // Cereza
+//	
+//	movz x15, 0xFF, lsl 16
+//	movk x15, 0x0000, lsl 00  // Rojo
+//
+//	movz x16, 0xFF, lsl 16
+//	movk x16, 0x9101, lsl 00  // Naranja
+//	
+//	movz x17, 0xFF, lsl 16
+//	movk x17, 0xFF00, lsl 00  // Amarillo
+//	
+//	movz x18, 0x34, lsl 16
+//	movk x18, 0xFF00, lsl 00  // Verde
+//	
+//	movz x19, 0x01, lsl 16
+//	movk x19, 0x98FF, lsl 00  // Celeste
+//	
+//	movz x20, 0x68, lsl 16
+//	movk x20, 0x32FF, lsl 00  // Violeta
+//	
+//	movz x21, 0x00, lsl 16
+//	movk x21, 0x0000, lsl 00  // Negro
+//	
+//	movz x22, 0xFF, lsl 16
+//	movk x22, 0xFFFF, lsl 00  // Blanco
 
 //PINTAR FONDO DE AZUL
-	mov x2, SCREEN_HEIGH         // Y Size 
-getFullX:
-	mov x1, SCREEN_WIDTH         // X Size
-paintBackground:
-	stur w10,[x0]	   // Set color of pixel N
-	add x0,x0,4	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,paintBackground	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
-	cbnz x2,getFullX	   // if not last row, jump
+//	mov x2, SCREEN_HEIGH         // Y Size 
+//getFullX:
+//	mov x1, SCREEN_WIDTH         // X Size
+//paintBackground:
+//	stur w10,[x0]	   // Set color of pixel N
+//	add x0,x0,4	   // Next pixel
+//	sub x1,x1,1	   // decrement X counter
+//	cbnz x1,paintBackground	   // If not end row jump
+//	sub x2,x2,1	   // Decrement Y counter
+//	cbnz x2,getFullX	   // if not last row, jump
 
 //CREMA DE LA GALLETA
 //Línea 1
-lineHeightCream1:
-	mov x2,7 // Line height in pixels
-setInitialPositionCream1:
-	movz x24, 0x07, lsl 16
-	movk x24, 0x358C, lsl 00 // 4 * (285 + (185 * 640)) - 4 * (640 - 70)
-	add x0,x23,x24
-newLineCream1:
-	mov x1,70 // Line width in pixels
-	add x0,x0,2280 // (640 - 70) * 4
-paintLineCream1:
-	stur w13,[x0]	   // Set color of pixel N
-	add x0,x0,4	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,paintLineCream1	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
-	cbnz x2,newLineCream1	   // if not last row, jump
-//Línea 2
-lineHeightCream2:
-	mov x2,5 // Line height in pixels
-setInitialPositionCream2:
-	movz x24, 0x07, lsl 16
-	movk x24, 0x7BA0, lsl 00 // 4 * (280 + (192 * 640)) - 4 * (640 - 80)
-	add x0,x23,x24
-newLineCream2:
-	mov x1,80 // Line width in pixels
-	add x0,x0,2240 // 4 * (640 - 80)
-paintLineCream2:
-	stur w13,[x0]	   // Set color of pixel N
-	add x0,x0,4	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,paintLineCream2	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
-	cbnz x2,newLineCream2	   // if not last row, jump
-//Línea 3
-lineHeightCream3:
-	mov x2,54 // Line height in pixels
-setInitialPositionCream3:
-	movz x24, 0x07, lsl 16
-	movk x24, 0xADB8, lsl 00 // 4 * (274 + (197 * 640)) - 4 * (640 - 92)
-	add x0,x23,x24
-newLineCream3:
-	mov x1,92 // Line width in pixels
-	add x0,x0,2192 // 4 * (640 - 92)
-paintLineCream3:
-	stur w13,[x0]	   // Set color of pixel N
-	add x0,x0,4	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,paintLineCream3	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
-	cbnz x2,newLineCream3	   // if not last row, jump
-//Linea 4
-lineHeightCream4:
-	mov x2,5 // Line height in pixels
-setInitialPositionCream4:
-	movz x24, 0x09, lsl 16
-	movk x24, 0xC9A0, lsl 00 // 4 * (280 + (251 * 640)) - 4 * (640 - 80)
-	add x0,x23,x24
-newLineCream4:
-	mov x1,80 // Line width in pixels
-	add x0,x0,2240 // 4 * (640 - 80)
-paintLineCream4:
-	stur w13,[x0]	   // Set color of pixel N
-	add x0,x0,4	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,paintLineCream4	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
-	cbnz x2,newLineCream4	   // if not last row, jump
-//Línea 5
-lineHeightCream5:
-	mov x2,7 // Line height in pixels
-setInitialPositionCream5:
-	movz x24, 0x09, lsl 16
-	movk x24, 0xFB8C, lsl 00 // 4 * (285 + (256 * 640)) - 4 * (640 - 70)
-	add x0,x23,x24
-newLineCream5:
-	mov x1,70 // Line width in pixels
-	add x0,x0,2280 // (640 - 70) * 4
-paintLineCream5:
-	stur w13,[x0]	   // Set color of pixel N
-	add x0,x0,4	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,paintLineCream5	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
-	cbnz x2,newLineCream5	   // if not last row, jump
-	
-	
-//------------------------------- ACA COPIO Y PEGO-------------------------------------------------------------------
+//mov x1,70 // Line width in pixels
+//mov x2,7 // Line height in pixels
+//mov x3,2280 // 4 * (640 - line width)
+//mov x4, x1 // Auxiliar variable
+//movz x24, 0x07, lsl 16
+//movk x24, 0x358C, lsl 00 // 4 * (285 + (185 * 640)) - x3
+//add x0,x23,x24 // Set initial position to x0
+//add w4,w13,wzr // Set color
+//newLine:
+//	mov x1,x4
+//	add x0,x0,x3
+//paintLine:
+//	stur w4,[x0]	   // Set color of pixel N
+//	add x0,x0,4	   // Next pixel
+//	sub x1,x1,1	   // decrement X counter
+//	cbnz x1,paintLine	   // If not end row jump
+//	sub x2,x2,1	   // Decrement Y counter
+//	cbnz x2,newLine	   // if not last row, jump
 
-
+//--------------------------------------------------------------------------------------probando hacer funciones----
+//esta parte sigue siendo main
 animation:
 		bl paintBackground
-	
-		
-		
-		
-		//linea superior negra lomo
-		movz x0, 273
-		movz x1, 180   //aca las coordenadas
-		movz x2, 92
-		movz x3, 5   //aca el ancho y largo
-		movz x4, 0x00, lsl 16
-	        movk x4, 0x0000, lsl 00 //lila
-		bl paintLine
-		
-		//otra linea negra de la cara 
-		movz x0, 322
-		movz x1, 235   //aca las coordenadas
-		movz x2, 5
-		movz x3, 26   //aca el ancho y largo
-		movz x4, 0x00, lsl 16
-	        movk x4, 0x0000, lsl 00 //lila
-		bl paintLine
-		
-		
-		//otra linea negra de la cara mas arriba
-		movz x0, 327
-		movz x1, 212   //aca las coordenadas
-		movz x2, 5
-		movz x3, 24   //aca el ancho y largo
-		movz x4, 0x00, lsl 16
-	        movk x4, 0x0000, lsl 00 //lila
-		bl paintLine
-		
-		
-		//linea negra grande lateral
-		movz x0, 263
-		movz x1, 192   //aca las coordenadas
-		movz x2, 5
-		movz x3, 80   //aca el ancho y largo
-		movz x4, 0x00, lsl 16
-	        movk x4, 0x0000, lsl 00 //lila
-		bl paintLine
-		
-		
-		
-		
-	
-	 //PUNTO NEGRO
-		movz x0, 268
-		movz x1, 186   //aca las coordenadas
-		movz x2, 5
-		movz x3, 6  //aca el ancho y largo
-		movz x4, 0x00, lsl 16
-	        movk x4, 0x0000, lsl 00 //lila
-		bl paintLine
-		
-		//rosa
-		movz x0, 268
-		movz x1, 192   //aca las coordenadas
-		movz x2, 5
-		movz x3, 80  //aca el ancho y largo
-		movz x4, 0xFF, lsl 16
-	        movk x4, 0xCB9B, lsl 00 //lila
-		bl paintLine
-		
-		//CREMA
-		mov x0, 271
-		movz x1, 202
-		movz x2, 92
-		movz x3, 54
-		movz x4, 0xFF, lsl 16
-	        movk x4, 0x98FF, lsl 00 //rosa que representa a nyan cat aunque no es lo que hace en el gif pero es de prueba
-		bl paintLine
-	
-	
-	
-		
+
+		mov x7, 176
+		bl paintPopTart
 		//-------------	--------------------------------------------------
 		// Loop infinito
-		//add x19, x19, #1
-		//mov x0, 0xFFFFF
+		add x19, x19, #1
+		mov x0, 0xFFFFF
 		//bl delay a definir porque algo anda mal
-		
+		//b animation
 	InfLoop: 
 		b InfLoop
 	
@@ -246,7 +106,7 @@ animation:
 		movz x2, 0x01, lsl 16
 	        movk x2, 0x3368, lsl 00   // azul
 		movz x1, 0xB000
-		movk x1, 0x4, LSL #16
+		movk x1, 0x4, lsl 16
 	
 	super_loop0:
 		stur x2,[x0]	   // Set color of pixel N
@@ -258,6 +118,7 @@ animation:
 	 
 	
 	paintLine: // x0: X, x1: Y, x2: W, x3: H, x4: Color
+	add x3, x3, #1
 	
 	paintLinefila: 
 		mov	   x5, SCREEN_WIDTH
@@ -276,6 +137,134 @@ animation:
 		cbnz x3, paintLinefila
 	
 		ret
+
+	paintPopTart:
+		//CONTORNO
+		movz x0, 274
+		mov x1, x7
+		movz x2, 92
+		movz x3, 5
+		movz x4, 0x00, lsl 16
+	        movk x4, 0x0000, lsl 00 //negro
+		bl paintLine //contorno arriba
+		add x1, x7, 91
+		movz x2, 92
+		movz x3, 5
+		bl paintLine //contorno abajo
+		movz x0, 269
+		add x1, x7, 5
+		movz x2, 5
+		movz x3, 5
+		bl paintLine //vertice arriba izquierda
+		add x1, x7, 85
+		movz x3, 5
+		bl paintLine //vertice abajo izquierda
+		movz x0, 366
+		add x1, x7, 5
+		movz x3, 5
+		bl paintLine //vertice arriba derecha
+		movz x0, 264
+		add x1, x7, 10
+		movz x3, 75
+		bl paintLine //contorno izquierda
+		movz x0, 371
+		add x1, x7, 10
+		movz x3, 75
+		bl paintLine //contorno derecha
+
+		//MASA
+		movz x0, 274
+		add x1, x7, 5
+		movz x2, 92
+		movz x3, 5
+		movz x4, 0xFF, lsl 16
+	        movk x4, 0xCB9B, lsl 00 //color masa
+		bl paintLine //linea de arriba
+		add x1, x7, 85
+		movz x3, 5
+		bl paintLine //linea de abajo
+		movz x0, 269
+		add x1, x7, 10
+		movz x2, 102
+		movz x3, 75
+		bl paintLine //cuerpo
+
+		//CREMA
+		movz x0, 285
+		add x1, x7, 10
+		movz x2, 70
+		movz x3, 5
+		movz x4, 0xFF, lsl 16
+	        movk x4, 0x98FF, lsl 00 //color crema
+		bl paintLine //linea de arriba 1
+		add x1, x7, 80
+		movz x3, 5
+		bl paintLine //linea de abajo 1
+		movz x0, 280
+		add x1, x7, 15
+		movz x2, 80
+		movz x3, 5
+		bl paintLine //linea de arriba 2
+		add x1, x7, 75
+		movz x3, 5
+		bl paintLine //linea de abajo 2
+		movz x0, 274
+		add x1, x7, 20
+		movz x2, 92
+		movz x3, 60
+		bl paintLine //cuerpo
+
+		//CEREZAS
+		movz x0, 285
+		add x1, x7, 20
+		movz x2, 5
+		movz x3, 5
+		movz x4, 0xFE, lsl 16
+	        movk x4, 0x329E, lsl 00 //color cereza
+		bl paintLine //cereza 1
+		movz x0, 312
+		add x1, x7, 16
+		movz x3, 5
+		bl paintLine //cereza 2
+		movz x0, 328
+		add x1, x7, 16
+		movz x3, 5
+		bl paintLine //cereza 3
+		movz x0, 350
+		add x1, x7, 26
+		movz x3, 6
+		bl paintLine //cereza 4
+		movz x0, 307
+		add x1, x7, 37
+		movz x3, 6
+		bl paintLine //cereza 5
+		movz x0, 290
+		add x1, x7, 48
+		movz x2, 6
+		movz x3, 5
+		bl paintLine //cereza 6
+		movz x0, 312
+		add x1, x7, 53
+		movz x2, 5
+		movz x3, 6
+		bl paintLine //cereza 7
+		movz x0, 279
+		add x1, x7, 59
+		movz x3, 5
+		bl paintLine //cereza 8
+		movz x0, 301
+		add x1, x7, 69
+		movz x2, 6
+		movz x3, 6
+		bl paintLine //cereza 9
+		movz x0, 285
+		add x1, x7, 75
+		movz x2, 5
+		movz x3, 5
+		bl paintLine //cereza 10
+
+		ret
+	
 
 
 
