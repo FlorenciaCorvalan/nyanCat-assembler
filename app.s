cc096b1b11c4b77aaaed56656aa670cd557dd562
Florencia <florcorvalan26@gmail.com>
2021-06-11 00:16:30 -0300

    l

--- app.s
+++ app.s
@@ -10,6 +10,161 @@ main:
  	mov x23, x0	// Save framebuffer base address to x20
  	
  	bl paintBackground
+ 	 
+ 	 movz x7, 176
+ 	//CONTORNO
+		movz x0, 274
+		mov x1, x7
+		movz x2, 92
+		movz x3, 5
+		movz x4, 0x00, lsl 16
+	        movk x4, 0x0000, lsl 00 //negro
+		bl paintLine //contorno arriba
+		add x1, x7, 91
+		movz x2, 92
+		movz x3, 5
+		bl paintLine //contorno abajo
+		movz x0, 269
+		add x1, x7, 5
+		movz x2, 5
+		movz x3, 5
+		bl paintLine //vertice arriba izquierda
+		add x1, x7, 85
+		movz x3, 5
+		bl paintLine //vertice abajo izquierda
+		movz x0, 366
+		add x1, x7, 5
+		movz x3, 5
+		bl paintLine //vertice arriba derecha
+		movz x0, 264
+		add x1, x7, 10
+		movz x3, 75
+		bl paintLine //contorno izquierda
+		movz x0, 371
+		add x1, x7, 10
+		movz x3, 75
+		bl paintLine //contorno derecha
+
+		//MASA
+		movz x0, 274
+		add x1, x7, 5
+		movz x2, 92
+		movz x3, 5
+		movz x4, 0xFF, lsl 16
+	        movk x4, 0xCB9B, lsl 00 //color masa
+		bl paintLine //linea de arriba
+		add x1, x7, 85
+		movz x3, 5
+		bl paintLine //linea de abajo
+		movz x0, 269
+		add x1, x7, 10
+		movz x2, 102
+		movz x3, 75
+		bl paintLine //cuerpo
+
+		//CREMA
+		movz x0, 285
+		add x1, x7, 10
+		movz x2, 70
+		movz x3, 5
+		movz x4, 0xFF, lsl 16
+	        movk x4, 0x98FF, lsl 00 //color crema
+		bl paintLine //linea de arriba 1
+		add x1, x7, 80
+		movz x3, 5
+		bl paintLine //linea de abajo 1
+		movz x0, 280
+		add x1, x7, 15
+		movz x2, 80
+		movz x3, 5
+		bl paintLine //linea de arriba 2
+		add x1, x7, 75
+		movz x3, 5
+		bl paintLine //linea de abajo 2
+		movz x0, 274
+		add x1, x7, 20
+		movz x2, 92
+		movz x3, 60
+		bl paintLine //cuerpo
+
+		//CEREZAS
+		movz x0, 285
+		add x1, x7, 20
+		movz x2, 5
+		movz x3, 5
+		movz x4, 0xFE, lsl 16
+	        movk x4, 0x329E, lsl 00 //color cereza
+		bl paintLine //cereza 1
+		movz x0, 312
+		add x1, x7, 16
+		movz x3, 5
+		bl paintLine //cereza 2
+		movz x0, 328
+		add x1, x7, 16
+		movz x3, 5
+		bl paintLine //cereza 3
+		movz x0, 350
+		add x1, x7, 26
+		movz x3, 6
+		bl paintLine //cereza 4
+		movz x0, 307
+		add x1, x7, 37
+		movz x3, 6
+		bl paintLine //cereza 5
+		movz x0, 290
+		add x1, x7, 48
+		movz x2, 6
+		movz x3, 5
+		bl paintLine //cereza 6
+		movz x0, 312
+		add x1, x7, 53
+		movz x2, 5
+		movz x3, 6
+		bl paintLine //cereza 7
+		movz x0, 279
+		add x1, x7, 59
+		movz x3, 5
+		bl paintLine //cereza 8
+		movz x0, 301
+		add x1, x7, 69
+		movz x2, 6
+		movz x3, 6
+		bl paintLine //cereza 9
+		movz x0, 285
+		add x1, x7, 75
+		movz x2, 5
+		movz x3, 5
+		bl paintLine //cereza 10
+		
+ 	
+ 	mov x7, 302
+ 	movz x8, 209
+ 	//CONTORNO
+		mov x0, x7
+		mov x1, x8
+		movz x2, 5
+		movz x3, 20
+		movz x4, 0x00, lsl 16
+	        movk x4, 0x0000, lsl 00 //negro
+		bl paintLine //lateral izq
+		
+		add x0, x7, 5
+		sub x1, x8, 6
+		movz x2, 11
+		movz x3, 6
+		bl paintLine //sup
+		
+		add x0, x7, 16
+		add x1, x8, 0
+		movz x2, 5
+		movz x3, 5
+		bl paintLine //vertice 
+		
+		add x0, x7, 21
+		add x1, x8, 5
+		movz x2, 6
+		movz x3, 6
+		bl paintLine //vertice abajo izquierda
 	
 	//PRIMERA COLUMNA ARCOIRIS
 		//rojo
@@ -298,6 +453,41 @@ main:
 	        movk x4, 0x32FF, lsl 00 
 		bl paintLine
 		
+		mov x7, 176
+		
+		
+		//tail1
+		mov x0, 231
+		movz x1, 213
+		movz x2, 6
+		movz x3, 16
+		movz x4, 0x00, lsl 16
+	        movk x4, 0x0000, lsl 00 
+		bl paintLine
+		
+		//tail2
+		mov x0, 237
+		movz x1, 213
+		movz x2, 16
+		movz x3, 6
+		movz x4, 0x00, lsl 16
+	        movk x4, 0x0000, lsl 00 
+		bl paintLine
+		
+		//tail3
+		mov x0, 247
+		movz x1, 219
+		movz x2, 11
+		movz x3, 5
+		movz x4, 0x00, lsl 16
+	        movk x4, 0x0000, lsl 00 
+		bl paintLine
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+	
+		
 		
 		
 	
@@ -309,12 +499,6 @@ main:
 	
 	
 	
-		
-		//-------------	--------------------------------------------------
-		// Loop infinito
-		//add x19, x19, #1
-		//mov x0, 0xFFFFF
-		//bl delay a definir porque algo anda mal
 		
 	InfLoop: 
 		b InfLoop
@@ -356,8 +540,137 @@ main:
 		cbnz x3, paintLinefila
 	
 		ret
+		
+//----------------------------------------------------------------------------------------------------------------------
 
 
-//r
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+catHead:
+		
+		movz x0, 366
+		add x1, x7, 5
+		movz x3, 5
+		bl paintLine //vertice arriba derecha
+		movz x0, 264
+		add x1, x7, 10
+		movz x3, 75
+		bl paintLine //contorno izquierda
+		movz x0, 371
+		add x1, x7, 10
+		movz x3, 75
+		bl paintLine //contorno derecha
+
+		//MASA
+		movz x0, 274
+		add x1, x7, 5
+		movz x2, 92
+		movz x3, 5
+		movz x4, 0xFF, lsl 16
+	        movk x4, 0xCB9B, lsl 00 //color masa
+		bl paintLine //linea de arriba
+		add x1, x7, 85
+		movz x3, 5
+		bl paintLine //linea de abajo
+		movz x0, 269
+		add x1, x7, 10
+		movz x2, 102
+		movz x3, 75
+		bl paintLine //cuerpo
+
+		//CREMA
+		movz x0, 285
+		add x1, x7, 10
+		movz x2, 70
+		movz x3, 5
+		movz x4, 0xFF, lsl 16
+	        movk x4, 0x98FF, lsl 00 //color crema
+		bl paintLine //linea de arriba 1
+		add x1, x7, 80
+		movz x3, 5
+		bl paintLine //linea de abajo 1
+		movz x0, 280
+		add x1, x7, 15
+		movz x2, 80
+		movz x3, 5
+		bl paintLine //linea de arriba 2
+		add x1, x7, 75
+		movz x3, 5
+		bl paintLine //linea de abajo 2
+		movz x0, 274
+		add x1, x7, 20
+		movz x2, 92
+		movz x3, 60
+		bl paintLine //cuerpo
+
+		//CEREZAS
+		movz x0, 285
+		add x1, x7, 20
+		movz x2, 5
+		movz x3, 5
+		movz x4, 0xFE, lsl 16
+	        movk x4, 0x329E, lsl 00 //color cereza
+		bl paintLine //cereza 1
+		movz x0, 312
+		add x1, x7, 16
+		movz x3, 5
+		bl paintLine //cereza 2
+		movz x0, 328
+		add x1, x7, 16
+		movz x3, 5
+		bl paintLine //cereza 3
+		movz x0, 350
+		add x1, x7, 26
+		movz x3, 6
+		bl paintLine //cereza 4
+		movz x0, 307
+		add x1, x7, 37
+		movz x3, 6
+		bl paintLine //cereza 5
+		movz x0, 290
+		add x1, x7, 48
+		movz x2, 6
+		movz x3, 5
+		bl paintLine //cereza 6
+		movz x0, 312
+		add x1, x7, 53
+		movz x2, 5
+		movz x3, 6
+		bl paintLine //cereza 7
+		movz x0, 279
+		add x1, x7, 59
+		movz x3, 5
+		bl paintLine //cereza 8
+		movz x0, 301
+		add x1, x7, 69
+		movz x2, 6
+		movz x3, 6
+		bl paintLine //cereza 9
+		movz x0, 285
+		add x1, x7, 75
+		movz x2, 5
+		movz x3, 5
+		bl paintLine //cereza 10
+		
 
 
